c9ff2f8ea71907a8d09634bf965c4398c9e73982
Dalia Vazquez <42613292+DaliaV@users.noreply.github.com>
2025-09-15 14:49:55 -0600

    Create beesave_currencies_inserts.sql

---
+++ pl_sql/beesave_currencies_inserts.sql
@@ -0,0 +1,122 @@
+-- bloque 1 (20 - fiat y stablecoins)
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('usd', 1, 'united states dollar. official currency of the united states and widely used as a global reference.', 'United States, Ecuador, El Salvador, Panama, Puerto Rico', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('usdt', 0.99938826, 'tether. stablecoin pegged to the united states dollar.', 'Global, cryptocurrency exchanges', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('usdc', 0.99655696, 'usd coin. fiat-backed stablecoin pegged to usd.', 'Global, cryptocurrency exchanges', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('eur', 0.85259194, 'euro. official currency of the eurozone.', 'Germany, France, Spain, Italy, Portugal, Netherlands, Belgium, Austria, Ireland, Greece', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('mxn', 18.44998597, 'mexican peso. official currency of mexico.', 'Mexico', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('cad', 1.38430178, 'canadian dollar. official currency of canada.', 'Canada', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('gbp', 0.73750551, 'british pound sterling. official currency of the united kingdom.', 'United Kingdom, Jersey, Guernsey, Isle of Man', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('jpy', 147.5783362, 'japanese yen. official currency of japan.', 'Japan', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('chf', 0.79658762, 'swiss franc. official currency of switzerland and liechtenstein.', 'Switzerland, Liechtenstein', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('brl', 5.35294283, 'brazilian real. official currency of brazil.', 'Brazil', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('ars', 1451.61870137, 'argentine peso. official currency of argentina.', 'Argentina', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('clp', 952.59205806, 'chilean peso. official currency of chile.', 'Chile', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('pen', 3.48763109, 'peruvian sol. official currency of peru.', 'Peru', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('cop', 3896.27541804, 'colombian peso. official currency of colombia.', 'Colombia', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('cny', 7.12327477, 'chinese yuan (renminbi). official currency of china.', 'China', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('inr', 88.26710751, 'indian rupee. official currency of india.', 'India', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('krw', 1389.9135425, 'south korean won. official currency of south korea.', 'South Korea', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('rub', 83.45008676, 'russian ruble. official currency of russia.', 'Russia', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('zar', 17.38699825, 'south african rand. official currency of south africa and some neighboring countries.', 'South Africa, Lesotho, Namibia, Eswatini', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('aud', 1.50248608, 'australian dollar. official currency of australia and small pacific states.', 'Australia, Nauru, Tuvalu, Kiribati', 'https://github.com/');
+
+-- bloque 2 (20 - crypto / tokens popular)
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('btc', 0.000008675, 'bitcoin. decentralized cryptocurrency and primary store of value.', 'Global, cryptocurrency exchanges', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('eth', 0.0002164786, 'ethereum (ether). smart contracts platform currency.', 'Global, defi, nfts, exchanges', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('bnb', 0.001077604, 'binance coin (bnb). native token of the binance ecosystem.', 'Global, binance ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('xrp', 0.32861392, 'xrp. payment-oriented token of the ripple network.', 'Global, financial partners', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('ada', 1.17022166, 'cardano (ada). proof-of-stake blockchain token.', 'Global, cardano ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('sol', 0.0041453554, 'solana (sol). high-throughput blockchain token.', 'Global, solana ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('doge', 3.58347035, 'dogecoin. meme cryptocurrency with wide retail recognition.', 'Global, cryptocurrency exchanges', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('shib', 72715.2370004, 'shiba inu (shib). erc-20 token inspired by meme coins.', 'Global, cryptocurrency exchanges', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('trx', 2.85890453, 'tron (trx). blockchain token for decentralized applications.', 'Global, tron ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('dot', 0.23073512, 'polkadot (dot). interoperability-focused blockchain token.', 'Global, polkadot ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('ltc', 0.0086438979, 'litecoin. peer-to-peer cryptocurrency designed for fast payments.', 'Global, cryptocurrency exchanges', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('bch', 0.0016624132, 'bitcoin cash. fork of bitcoin for faster and cheaper transactions.', 'Global, cryptocurrency exchanges', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('avax', 0.033728975, 'avalanche (avax). platform token for dapps and defi.', 'Global, avalanche ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('usdp', 0.99925056, 'pax dollar (usdp). regulated stablecoin pegged to usd.', 'Global, cryptocurrency exchanges', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('dai', 0.99994577, 'dai. decentralized stablecoin collateralized on ethereum.', 'Global, defi ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('busd', 1.00005628, 'binance usd (busd). stablecoin issued within the binance ecosystem.', 'Global, binance ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('egld', 0.067489071, 'multiversx / elrond (egld). blockchain token for scalable dapps.', 'Global, multiversx ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('atom', 0.21539006, 'cosmos (atom). token for blockchain interoperability.', 'Global, cosmos ecosystem', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('link', 0.041394648, 'chainlink (link). decentralized oracle network token.', 'Global, oracles, defi', 'https://github.com/');
+
+insert into beesave_currencies (name, us_val, description, region_list, icon_link)
+values ('uni', 0.10594563, 'uniswap (uni). governance token for the uniswap dex.', 'Global, defi, exchanges', 'https://github.com/');
+
